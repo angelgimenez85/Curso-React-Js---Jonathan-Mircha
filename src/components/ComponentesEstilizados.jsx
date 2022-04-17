6f50b1a75f43c72b9fef82c06b82d840d51300d8
angelgimenez85 <gimenezangel84@gmail.com>
2022-04-16 23:14:42 -0300

    Styled Components

--- src/components/ComponentesEstilizados.jsx
+++ src/components/ComponentesEstilizados.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled, {
+  css,
+  keyframes,
+  ThemeProvider,
+  createGlobalStyle,
+} from 'styled-components';
 
 const mainColor = '#db7093';
 const mainAlphaColor80 = '#db709380';
+
 const setTransitionTime = (time) => `all ${time} ease-in-out;`;
+
+const fadein = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
 const MyH3 = styled.h3`
   padding: 2rem;
   text-align: center;
@@ -12,8 +28,9 @@ const MyH3 = styled.h3`
   color: ${({ color }) => color || '#000'};
   background-color: ${mainColor};
   transition: ${setTransitionTime('1s')};
+  animation: ${fadein} 2s ease-out;
 
-  ${({isButton}) =>
+  ${({ isButton }) =>
     isButton &&
     css`
     margin: auto;
@@ -27,15 +44,60 @@ const MyH3 = styled.h3`
   }
 `;
 
+const light = {
+  color: '#222',
+  bgColor: '#DDD',
+};
+
+const dark = {
+  color: '#DDD',
+  bgColor: '#222',
+};
+
+const Box = styled.div`
+  padding: 1rem;
+  margin: 1rem;
+  color: ${({ theme }) => theme.color};
+  background-color: ${({ theme }) => theme.bgColor};
+`;
+
+// Atributos heredados
+const BoxRounded = styled(Box)`
+  border-radius: 1rem;
+`;
+
+//Atributos globales (iría en index o App)
+const GlobalStyle = createGlobalStyle`
+  h2 {
+    padding: 2rem;
+    background-color: #fff;
+    color: #61dafb;
+    text-transform: uppercase;
+  }
+`;
+
 export default function ComponentesEstilizados() {
-
-
   return (
     <>
+      <GlobalStyle/>
       <h2>Styled-Components</h2>
       <MyH3>Hola, soy un h3 estilizado</MyH3>
       <MyH3 color="#61dafb">Hola, soy un h3 estilizado</MyH3>
-      <MyH3 isButton color="#ffffff">Soy un h3 estilizado como botón</MyH3>
+      <MyH3 isButton color="#ffffff">
+        Soy un h3 estilizado como botón
+      </MyH3>
+      <ThemeProvider theme={light}>
+        <Box>Soy una caja light</Box>
+      </ThemeProvider>
+      <ThemeProvider theme={dark}>
+        <Box>Soy una caja dark</Box>
+      </ThemeProvider>
+      <ThemeProvider theme={light}>
+        <BoxRounded>Soy una caja light redondeada</BoxRounded>
+      </ThemeProvider>
+      <ThemeProvider theme={dark}>
+        <BoxRounded>Soy una caja dark redondeada</BoxRounded>
+      </ThemeProvider>
     </>
   );
 }
