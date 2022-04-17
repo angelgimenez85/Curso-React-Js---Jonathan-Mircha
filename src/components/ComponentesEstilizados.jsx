e37c662b153ec003346fad1041f7f52a06837b52
angelgimenez85 <gimenezangel84@gmail.com>
2022-04-16 22:35:20 -0300

    Styled Components

--- src/components/ComponentesEstilizados.jsx
+++ src/components/ComponentesEstilizados.jsx
@@ -1,39 +1,41 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
-export default function ComponentesEstilizados() {
-  const mainColor = '#db7093';
-  const mainAlphaColor80 = '#db709380';
-  const setTransitionTime = (time) => `all ${time} easy-in-out;`;
-  const MyH3 = styled.h3`
-    padding: 2rem;
-    text-align: center;
-    color: ${(props) => props.color};
-    color: ${({ color }) => color};
-    color: ${({ color }) => color || '#000'}
-    background-color: ${mainColor};
-    transition: ${setTransitionTime('1s')};
+const mainColor = '#db7093';
+const mainAlphaColor80 = '#db709380';
+const setTransitionTime = (time) => `all ${time} ease-in-out;`;
+const MyH3 = styled.h3`
+  padding: 2rem;
+  text-align: center;
+  color: ${(props) => props.color};
+  color: ${({ color }) => color};
+  color: ${({ color }) => color || '#000'};
+  background-color: ${mainColor};
+  transition: ${setTransitionTime('1s')};
 
-    ${(props) =>
-      props.isButton &&
-      css`
-      margin: auto;
-      max-width: 50%;
-      border-radius: 0.25rem;
-      cursor: pointer;
-    `}
-    
-    &:hover {
-      background-color: ${mainAlphaColor80}      
-    }
-  `;
+  ${({isButton}) =>
+    isButton &&
+    css`
+    margin: auto;
+    max-width: 50%;
+    border-radius: 0.25rem;
+    cursor: pointer;
+  `}
+  
+  &:hover {
+    background-color: ${mainAlphaColor80}      
+  }
+`;
+
+export default function ComponentesEstilizados() {
+
 
   return (
     <>
       <h2>Styled-Components</h2>
       <MyH3>Hola, soy un h3 estilizado</MyH3>
       <MyH3 color="#61dafb">Hola, soy un h3 estilizado</MyH3>
-      <MyH3 isButton>Soy un h3 estilizado como botón</MyH3>
+      <MyH3 isButton color="#ffffff">Soy un h3 estilizado como botón</MyH3>
     </>
   );
 }
